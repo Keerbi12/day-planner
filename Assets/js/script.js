50a34d0c83b65caa22307293f7c47170eaec2051
Keerbi Liu <kirby.liu12@gmail.com>
2021-07-22 17:26:18 +1000

    Added current date to top of page with moment.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,13 @@
 const m = moment();
 
-console.log(m.format())
+console.log(m.format('MMMM Do YYYY, h:mm:ss a'));
+
+// jQuery of var currentDay = getElementById('#currentDay'), 
+var currentDay = $('#currentDay');
+// Set the text to current date via moment.js
+currentDay.text(m.format('MMMM Do YYYY'));
+
+
+
+
 
